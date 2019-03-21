3b6dc81d8d07b82d3c12f65b4de2a5731343335c
Mup0c <mir-os@mail.ru>
2019-03-21 17:08:13 +1000

    lab 2 save

--- lab2.cpp
+++ lab2.cpp
@@ -1,4 +1,59 @@
-//
-// Created by Admin on 2019-03-21.
-//
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[])
+{
+    int count = 0;
+#pragma omp parallel reduction (+: count)
+    {
+        count++;
+        printf("Текущее значение count: %d\n", count);
+    }
+    printf("Число нитей: %d\n", count);
+
+
+    int A[100], i, n;
+    int sum = 0;
+/* Заполним исходные массивы */
+    for (i=0; i<100; i++){
+        A[i]=1;
+    }
+#pragma omp parallel shared(A) private(i, n) reduction (+: sum)
+    {
+/* Получим номер текущей нити */
+        n=omp_get_thread_num();
+#pragma omp for
+        for (i=0; i<100; i++)
+        {
+            sum += A[i];
+            printf("Нить %d сложила элементы с номером %d\n", n, i);
+        }
+    }
+    printf("%d\n",sum);
+
+    sum = 0;
+#pragma omp parallel
+    {
+        int local_sum = 0;
+#pragma omp for
+        for (int i = 0; i < 100; ++i)
+            local_sum += A[i];
+#pragma omp atomic
+        sum += local_sum;
+    }
+    printf("%d\n",sum);
+
+    sum = 0;
+#pragma omp parallel
+    {
+
+        #pragma omp for
+        for (int i = 0; i < 100; ++i)
+            #pragma omp critical
+                    sum += A[i];
+
+    }
+    printf("%d\n",sum);
+}
+
 
